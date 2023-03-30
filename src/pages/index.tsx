94b97b90a4e2aa7b97148fd0f2f92b5c21f916b7
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2023-03-31 00:44:49 +0200

    Layout first iteration of scroll component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,21 @@ import NavBar from '@/components/navbar'
 import Head from 'next/head'
 
 function Scroll() {
+  const content = () => {
+    const items = [];
+    for (let i = 0; i < 10; i++) {
+      items.push(<p className='text-zinc-50'>این یک جمله‌ی تست است این یک جمله‌ی تست است این یک جمله‌ی تست است این یک جمله‌ی تست است این یک جمله‌ی تست است این یک جمله‌ی تست است این یک جمله‌ی تست است این یک جمله‌ی تست است این یک جمله‌ی تست است این یک جمله‌ی تست است این یک جمله‌ی تست است این یک جمله‌ی تست است این یک جمله‌ی تست است </p>);
+    }
+    return items;
+  }
   return (
     <>
-      <main className='w-full px-8 lg:px-16 pt-8 pb-0'>
-
+      <main className='w-full flex items-center justify-center px-8 lg:px-16 pt-8'>
+        <div className='max-w-full md:max-w-md lg:max-w-xl max-h-96 overflow-y-auto'>
+          <div className='flex flex-col gap-y-4'>
+            {content()}
+          </div>
+        </div>
       </main>
     </>
   )
