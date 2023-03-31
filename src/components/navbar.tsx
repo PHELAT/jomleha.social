9e6797e66037728355cb4d15a2be3c2ba30ae19a
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2023-03-31 22:29:10 +0200

    Remove navbar logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,15 +2,12 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function NavBar() {
-    return (
-        <>
-          <header className='w-full'>
-            <div className="w-full px-8 lg:px-16 pt-8 pb-0 flex flex-row justify-between items-center">
-                <Link href={"/"}>
-                  <Image src="/jomleha.svg" alt="لوگوی جمله‌ها" width="36" height="36" />
-                </Link>
-            </div>
-          </header>
-        </>
-    );
+  return (
+    <>
+      <header className='w-full'>
+        <div className="w-full px-8 lg:px-16 pt-8 pb-0 flex flex-row justify-between items-center">
+        </div>
+      </header>
+    </>
+  );
 }
