b8f8007ff56ed11457ea088c7e07dfa45ddcd73c
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2023-04-01 18:43:55 +0200

    Remove navbar component

--- src/pages/jomleh/[id]/index.tsx
+++ src/pages/jomleh/[id]/index.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router';
 import Head from "next/head";
-import NavBar from "@/components/navbar";
 import Footer from "@/components/footer";
 
 function Jomleh(content: string) {
@@ -18,11 +17,9 @@ interface ScrollComponent {
 function Scroll(props: ScrollComponent) {
     return (
         <>
-            <main className='w-full flex items-center justify-center px-8 lg:px-16 pt-8'>
-                <div className='max-w-full md:max-w-md lg:max-w-xl max-h-96 overflow-y-auto'>
-                    <div className='flex flex-col gap-y-4'>
-                        {Jomleh(props.content)}
-                    </div>
+            <main className='w-full flex items-center justify-center px-8 lg:px-16'>
+                <div className='max-w-full md:max-w-md lg:max-w-xl overflow-y-auto'>
+                    {Jomleh(props.content)}
                 </div>
             </main>
         </>
@@ -40,8 +37,7 @@ export default function JomlehPage() {
                 <meta name="description" content="Jomleha" />
             </Head>
             <div className='flex flex-col h-screen justify-between'>
-                <NavBar />
-                <Scroll content={id}/>
+                <Scroll content={id} />
                 <Footer />
             </div>
         </>
