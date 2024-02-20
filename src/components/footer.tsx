eebacd09f9676fcc671eb14c529405006d72e50b
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2024-02-20 19:50:27 +0100

    Refactor footer.tsx for better performance, readability and maintainability

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,29 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export type FooterLink = "/" | "/about";
-
-export interface FooterProps {
-  link: FooterLink;
-  showIcons: boolean;
-}
-
 export default function Footer({ link, showIcons }: FooterProps) {
-  const footerIcons = () => {
-    const content = [];
-    for (let i = 0; i < 8; i++) {
-      content.push(
-        <Image
-          src="/jomleha.svg"
-          alt="لوگوی جمله‌ها"
-          width="12"
-          height="12"
-          key={`footericon${i}`}
-        />,
-      );
-    }
-    return content;
-  };
   const footerJustify = showIcons ? "justify-between" : "justify-start";
   return (
     <>
@@ -31,21 +9,55 @@ export default function Footer({ link, showIcons }: FooterProps) {
         <div
           className={`w-full h-24 flex flex-row items-center px-8 lg:px-16 ${footerJustify}`}
         >
-          <a href={link}>
-            <div className="grid grid-cols-4 gap-1">{footerIcons()}</div>
-          </a>
-          {showIcons && (
-            <div className="flex flex-row gap-4 items-center">
-              <Link href="https://github.com/PHELAT/jomleha.social">
-                <Image src="/github.svg" alt="انبار" width={24} height={24} />
-              </Link>
-              <Link href="https://twitter.com/jomleha_social">
-                <Image src="/twitter.svg" alt="توئیتر" width={24} height={24} />
-              </Link>
-            </div>
-          )}
+          <JomlehaIcon href={link} />
+          {showIcons && <JomlehaExternalLinks />}
         </div>
       </footer>
     </>
   );
 }
+
+function JomlehaIcon({ href }: { href: FooterLink }) {
+  const titleTag = href === "/about" ? "About Jomleha" : "Home page";
+  return (
+    <Link href={href} title={titleTag}>
+      <div className="grid grid-cols-4 gap-1">
+        {[...Array(8)].map((_, index) => (
+          <Image
+            key={index}
+            src="/jomleha.svg"
+            alt="logo"
+            width={12}
+            height={12}
+            priority={true}
+          />
+        ))}
+      </div>
+    </Link>
+  );
+}
+
+function JomlehaExternalLinks() {
+  return (
+    <div className="flex flex-row gap-4 items-center">
+      <Link href="https://github.com/PHELAT/jomleha.social">
+        <Image
+          src="/github.svg"
+          alt="Github repository"
+          width={24}
+          height={24}
+        />
+      </Link>
+      <Link href="https://twitter.com/jomleha_social">
+        <Image src="/twitter.svg" alt="Twitter" width={24} height={24} />
+      </Link>
+    </div>
+  );
+}
+
+export type FooterLink = "/" | "/about";
+
+export type FooterProps = {
+  link: FooterLink;
+  showIcons: boolean;
+};
